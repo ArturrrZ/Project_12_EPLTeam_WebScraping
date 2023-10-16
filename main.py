09170892b3be619f714c04475bc2fda9888b97c0
Artur Z <arturziianbaev@gmail.com>
2023-10-16 14:31:32 -0700

    open website and get html text

---
+++ main.py
@@ -0,0 +1,16 @@
+import requests
+from bs4 import BeautifulSoup
+
+###STEP 1: open website and get html text
+
+ARSENAL_FLASHSCORE='https://www.flashscore.com/team/arsenal/hA1Zm19f/squad/'
+header={
+    "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36 Edg/115.0.1901.203",
+    "Accept-Language":"en-US,en;q=0.9",
+}
+
+website=requests.get(url=ARSENAL_FLASHSCORE, headers=header)
+website.raise_for_status()
+print(website.status_code)
+website_html=website.text
+# print(website_html)
